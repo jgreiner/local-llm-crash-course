583b8afcd68ef7abc59d0fab600702b22fca17a1
Jochen Greiner <jochen@greiner.de>
2024-09-26 11:44:54 +0200

    add chainlit

--- chat.py
+++ chat.py
@@ -1,16 +1,11 @@
-from os import system
 from typing import List
+import chainlit as cl
 
 from ctransformers import AutoModelForCausalLM
 
-llm = AutoModelForCausalLM.from_pretrained(
-    "zoltanctoth/orca_mini_3B-GGUF",
-    model_file="orca-mini-3b.q4_0.gguf"
-)
 
-
-def get_prompt(instruction: str, history: List[str] = None) -> str:
-    system = "You are an AI assistant that gives short answers."
+def get_prompt(instruction: str, history: List[str] = None, answer_kind: str = "short") -> str:
+    system = "You are an AI assistant that gives " + answer_kind + " answers."
     prompt = f"### System:\n{system}\n\n### User:\n"
     if history is not None:
         prompt += f"This is the conversation history: {''.join(history)}. Now answer thr question: "
@@ -18,6 +13,30 @@ def get_prompt(instruction: str, history: List[str] = None) -> str:
     print(prompt)
     return prompt
 
+
+@cl.on_chat_start
+def on_chat_start():
+    global llm
+    llm = AutoModelForCausalLM.from_pretrained(
+        "zoltanctoth/orca_mini_3B-GGUF",
+        model_file="orca-mini-3b.q4_0.gguf"
+    )
+    print("A new chat session has started!")
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    msg = cl.Message(content="")
+    await msg.send()
+
+    prompt = get_prompt(message.content, None, "expensive")
+    for word in llm(prompt, stream=True):
+        print(word)
+        await msg.stream_token(word)
+    await msg.update()
+
+
+"""
 history = []
 
 answer = ""
@@ -35,3 +54,4 @@ for word in llm(get_prompt(question, history), stream=True):
     print(word,flush=True,end="")
     answer += word
 print()
+"""
---
+++ run-chainlit.sh
@@ -0,0 +1,1 @@
+chainlit run chat.py -w
